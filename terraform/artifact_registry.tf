f2f6a180c96b19570290d3efaf4641dc2d424131
Karthi <karthiamar07@gmail.com>
2026-08-20 11:57:19 +0530

    Fixed error in artifact_regirstry.tf file

--- terraform/artifact_registry.tf
+++ terraform/artifact_registry.tf
@@ -1,9 +1,9 @@
 resource "google_artifact_registry_repository" "webapp" {
     project = var.project_id
     location = var.region
-    respoitory_id = var.respoitory_id
+    repository_id = var.repository_id
     format = "DOCKER"
-    description = "Docker images for hte devops webapp"
+    description = "Docker images for the devops webapp"
 
     depends_on = [google_project_service.artifact_registry]
 }
