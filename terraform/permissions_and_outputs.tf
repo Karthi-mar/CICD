1f96542291479938de3931b829585ce0bcfc3634
Karthi <karthiamar07@gmail.com>
2026-08-20 17:13:34 +0530

    Added permissions_and_outputs.tf

---
+++ terraform/permissions_and_outputs.tf
@@ -0,0 +1,29 @@
+resource "google_project_iam_member" "artifact_registry_writer" {
+  project = var.project_id
+  role    = "roles/artifactregistry.writer"
+  member  = "serviceAccount:${google_service_account.github_deployer.email}"
+}
+
+resource "google_project_iam_member" "cloud_run_admin" {
+  project = var.project_id
+  role    = "roles/run.admin"
+  member  = "serviceAccount:${google_service_account.github_deployer.email}"
+}
+
+resource "google_project_iam_member" "act_as" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.github_deployer.email}"
+}
+
+output "workload_identity_provider" {
+  value = google_iam_workload_identity_pool_provider.github_provider.name
+}
+
+output "service_account_email" {
+  value = google_service_account.github_deployer.email
+}
+
+output "artifact_registry_repo" {
+  value = "${var.region}-docker.pkg.dev/${var.project_id}/${var.repository_id}"
+}
